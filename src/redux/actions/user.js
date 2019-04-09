b1079a6da7c8e020a171847c5058d7d286f42d7c
Paul Vazquez <pev@live.com.mx>
2019-04-09 15:24:32 -0500

    Added Backend Integration to fetchWorkers

--- src/redux/actions/user.js
+++ src/redux/actions/user.js
@@ -1,48 +1,74 @@
-import { LOADED_WORKERS, SELECT_WORKER, SELECT_DEVICE, LOADED_DEVICES } from "../../constants";
-import axios from 'axios';
-const linkGet = "https://d31e1bb5-30af-411e-9746-26902dd9fc3a.mock.pstmn.io";
+import {
+  LOADED_WORKERS,
+  SELECT_WORKER,
+  SELECT_DEVICE,
+  LOADED_DEVICES
+} from "../../constants";
+import axios from "axios";
+const linkBack = "https://d31e1bb5-30af-411e-9746-26902dd9fc3a.mock.pstmn.io";
+
+const backedOn = false;
+const workers = [
+  {
+    id: 23568428,
+    name: "Eduardo Angulo Martinez",
+    Lugar: "Molino Caliente 2",
+    Area: "Aceria"
+  },
+  {
+    id: 11821596,
+    name: "Jose Gonzalez Gonzalez",
+    Lugar: "Patio Pegi",
+    Area: "Aceria"
+  }
+];
 export const fetchWorkers = () => {
   return (dispatch, getState) => {
     const { auth } = getState();
     const user = auth.id;
-    axios.get(linkGet+"/trabajadores?username="+user)
-    .then(function(response){
+    if (backedOn) {
+      //FETCH workers from the api  here
+      axios
+        .get(linkBack + "/trabajadores?username=" + user)
+        .then(function(response) {
+          dispatch({
+            type: LOADED_WORKERS,
+            payload: response.data
+          });
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    } else {
       dispatch({
         type: LOADED_WORKERS,
-        payload: response.data
+        payload: workers
       });
-    })
-    .catch(function(error){
-      console.log(error);
-    })
-    //FETCH workers from the api  here
-
-   
+    }
   };
 };
 
 export const fetchDevices = () => {
-    return (dispatch, getState) => {
-      
-      //FETCH workers from the api  here
-      let devices = [];
-      for (var i = 0; i < 10; i++) {
-        let device = "Device " + Math.random() * 100;
-        devices.push({ id: i, name: device });
-      }
-      dispatch({
-        type: LOADED_DEVICES,
-        payload: devices
-      });
-    };
+  return (dispatch, getState) => {
+    //FETCH workers from the api  here
+    let devices = [];
+    for (var i = 0; i < 10; i++) {
+      let device = "Device " + Math.random() * 100;
+      devices.push({ id: i, name: device });
+    }
+    dispatch({
+      type: LOADED_DEVICES,
+      payload: devices
+    });
   };
+};
 
-export const setSelectedWorker = (id) =>({
-    type:SELECT_WORKER,
-    payload:id
-})
+export const setSelectedWorker = id => ({
+  type: SELECT_WORKER,
+  payload: id
+});
 
-export const setSelectedDevice = (id) =>({
-    type: SELECT_DEVICE,
-    payload: id
-})
+export const setSelectedDevice = id => ({
+  type: SELECT_DEVICE,
+  payload: id
+});
