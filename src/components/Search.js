728a361301f344af9a656a51da963604d3e8408a
Paul Vazquez <pev@live.com.mx>
2019-04-11 16:25:47 -0500

    Added Search bar placeholder to userAdminPage

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,52 @@
-import React from 'react';
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import InputBase from "@material-ui/core/InputBase";
+import Divider from "@material-ui/core/Divider";
+import IconButton from "@material-ui/core/IconButton";
+import AddIcon from "@material-ui/icons/Add";
+import SearchIcon from "@material-ui/icons/Search";
+import DirectionsIcon from "@material-ui/icons/Directions";
 
+const styles = {
+  root: {
+    padding: "2px 4px",
+    display: "flex",
+    alignItems: "center",
+    width:"80%",
+    margin:"auto",
+    marginTop: "2%",
+  },
+  input: {
+    marginLeft: 8,
+    flex: 1
+  },
+  iconButton: {
+    padding: 10,
+    color:"primary"
+  },
+  divider: {
+    width: 1,
+    height: 28,
+    margin: 4
+  }
+};
 
-class Search extends React.Component{
-
-    render(){
-        return(<p>Search</p>);
-    }
+class Search extends React.Component {
+  render() {
+    return (
+      <Paper style={styles.root} elevation={1}>
+        <IconButton style={styles.iconButton} color="primary" aria-label="Menu">
+          <AddIcon />
+        </IconButton>
+        <InputBase style={styles.input} placeholder="Buscar usuarios" />
+        <IconButton style={styles.iconButton} aria-label="Search">
+          <SearchIcon />
+        </IconButton>
+      </Paper>
+    );
+  }
 }
 
 export default Search;
--- src/pages/userAdminPage.js
+++ src/pages/userAdminPage.js
@@ -1,14 +1,16 @@
 import React from "react";
-import { Typography, Paper } from "@material-ui/core/";
+import { Typography, Paper, Fab } from "@material-ui/core/";
 import ExpansionPanel from "@material-ui/core/ExpansionPanel";
 import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
 import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-
+import Search from "../components/Search";
+import Add from "@material-ui/icons/Add";
 const styles = {
   main: {
     textAlign: "center",
-    flex: 1
+    flex: 1,
+    minHeight: 200
   },
   paper: {
     width: "80%",
@@ -16,32 +18,49 @@ const styles = {
     margin: "auto",
     marginTop: "2%"
   },
+  search: {},
   heading: {
-    flexBasis: '33.33%',
-    flexShrink: 0,
+    flexBasis: "33.33%",
+    flexShrink: 0
   },
+  fab: {
+    margin: 0,
+    top: "auto",
+    right: "10%",
+    bottom: "15%",
+    left: "auto",
+    position: "fixed"
+  }
 };
 
 class userAdminPage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      users: [1, 2, 3, 4]
+      users: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     };
   }
 
+  addUser = () => {
+    console.log("add user");
+  };
+
   renderUsers = () => {
-    return this.state.users.map((user,index) => (
+    return this.state.users.map((user, index) => (
       <ExpansionPanel>
         <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-          <Typography style={styles.heading} variant="title">Usuario {index}</Typography>
-          <Typography style={styles.heading} variant="subtitle1">Supervisor de Seguridad</Typography>
-          <Typography style={styles.heading} variant="subtitle1">Aceria</Typography>
+          <Typography style={styles.heading} variant="title">
+            Usuario {index}
+          </Typography>
+          <Typography style={styles.heading} variant="subtitle1">
+            Supervisor de Seguridad
+          </Typography>
+          <Typography style={styles.heading} variant="subtitle1">
+            Aceria
+          </Typography>
         </ExpansionPanelSummary>
         <ExpansionPanelDetails>
-          <Typography>
-           Tarjeta de edicion
-          </Typography>
+          <Typography>Tarjeta de edicion</Typography>
         </ExpansionPanelDetails>
       </ExpansionPanel>
     ));
@@ -51,8 +70,12 @@ class userAdminPage extends React.Component {
       <div>
         <div style={styles.main}>
           <Typography variant="h5">User Administration</Typography>
+          <Search style={styles.search} />
           <Paper style={styles.paper}>{this.renderUsers()}</Paper>
         </div>
+        <Fab style={styles.fab} color="primary" onClick={this.AddUser}>
+          <Add />
+        </Fab>
       </div>
     );
   }
