4e9622be860b6ed56a613e6fe5482891afcc1eff
Paul Vazquez <pev@live.com.mx>
2019-04-11 16:45:34 -0500

    Added Enter to LoginPage

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -2,26 +2,25 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { logIn } from "../redux/actions/auth";
 import { withRouter } from "react-router-dom";
-import { Button} from "@material-ui/core/";
+import { Button } from "@material-ui/core/";
 import { DASHBOARD, USER_ADMIN } from "../constants/routes";
 import { TextField, Typography } from "@material-ui/core";
 import Grid from "@material-ui/core/Grid";
 
 class LoginPage extends Component {
-  state={
-    username:null,
-    password:null
+  state = {
+    username: null,
+    password: null
   };
-  redirectOnAlreadyLogged = () =>{
-    if(this.props.auth.rol==="SU"){
+  redirectOnAlreadyLogged = () => {
+    if (this.props.auth.rol === "SU") {
       console.log("GOES TO SU");
       console.log(USER_ADMIN);
-      this.props.history.push('/user_admin');
-    }
-    else if (this.props.auth.username) {
+      this.props.history.push("/user_admin");
+    } else if (this.props.auth.username) {
       this.props.history.push(DASHBOARD);
     }
-  }
+  };
   componentDidMount() {
     //Checkc if redirect to dashboard needed
     //get cookie
@@ -34,52 +33,72 @@ class LoginPage extends Component {
     this.redirectOnAlreadyLogged();
   }
   handleChange = name => event => {
-  this.setState({[name]:event.target.value});
+    this.setState({ [name]: event.target.value });
   };
   render() {
     return (
       <form>
-        <Grid container direction="column" justify="flex-start" spacing={16} alignItems="center">
-        <div style={{width: "60%", textAlign:"center"}}>
-        <Grid item>
-        <Typography style={{paddingTop:20}}variant="h6">Sign In</Typography>
+        <Grid
+          container
+          direction="column"
+          justify="flex-start"
+          spacing={16}
+          alignItems="center"
+        >
+          <div style={{ width: "60%", textAlign: "center" }}>
+            <Grid item>
+              <Typography style={{ paddingTop: 20 }} variant="h6">
+                Sign In
+              </Typography>
+            </Grid>
+            <Grid item>
+              <TextField
+                id="username"
+                label="Username"
+                placeholder="ex: A00819877"
+                fullWidth
+                margin="normal"
+                variant="outlined"
+                onChange={this.handleChange("username")}
+              />
+            </Grid>
+            <Grid item>
+              <TextField
+                id="password"
+                label="Password"
+                type="password"
+                fullWidth
+                margin="normal"
+                variant="outlined"
+                onChange={this.handleChange("password")}
+                onKeyPress={ev => {
+                  if (ev.key === "Enter") {
+                    ev.preventDefault();
+                    this.redirectOnLogIn();
+                  }
+                }}
+              />
+            </Grid>
+            <Grid item>
+              <Button
+                variant="contained"
+                size="large"
+                color="primary"
+                style={{
+                  width: "50%",
+                  justify: "center"
+                }}
+                onClick={() => this.redirectOnLogIn()}
+              >
+                LogIN
+              </Button>
+              {this.props.auth.failed && (
+                <p color="#FF0000">Invalid Credentials</p>
+              )}
+            </Grid>
+          </div>
         </Grid>
-          <Grid item>
-           
-            <TextField
-          id="username"
-          label="Username"
-          placeholder="ex: A00819877"
-          fullWidth
-          margin="normal"
-          variant="outlined"
-          onChange={this.handleChange("username")}
-        />
-        </Grid>
-        <Grid item>
-            
-            <TextField
-          id="password"
-          label="Password"
-          type="password"
-          fullWidth
-          margin="normal"
-          variant="outlined"
-          onChange={this.handleChange("password")}
-        />
-        </Grid >
-        <Grid item>
-            <Button variant="contained" size="large" color="primary" style={{
-              width:"50%",
-              justify:"center"
-            }} onClick={() => this.redirectOnLogIn()}
-             >LogIN</Button  >
-             {this.props.auth.failed && <p color="#FF0000">Invalid Credentials</p>}
-        </Grid>    
-            </div>
-        </Grid>
-        
-        </form>
+      </form>
     );
   }
 }
@@ -88,7 +107,7 @@ const mapStateToProps = state => ({
 });
 const mapDispatchToProps = dispatch => {
   return {
-    logIn: (userInfo) => {
+    logIn: userInfo => {
       dispatch(logIn(userInfo));
     }
   };
