5211127d90e9badfa1956fc4f897d74038d151bd
Aurora Tijerina <auro.tj@gmail.com>
2019-04-28 13:34:25 -0500

    Fixed some format details and added the handle methods in the EditUserCard.js component.

--- src/components/EditUserCard.js
+++ src/components/EditUserCard.js
@@ -8,6 +8,13 @@ import TableRow from '@material-ui/core/TableRow';
 import TextField from '@material-ui/core/TextField';
 import MenuItem from '@material-ui/core/MenuItem';
 import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
+import FormControl from '@material-ui/core/FormControl';
 
 import save from "./images/save"
 
@@ -64,110 +71,142 @@ class userCard extends React.Component{
         lugar: this.props.lugar,
         area: this.props.area,
         puesto: this.props.puesto,
-        id: this.props.id,
+        username: this.props.username,
+        userpassword: this.props.userpassword,
+
         enableSave: false,
+        showPassword: false,
         };
 
     handleChange = prop => event => {
-        this.setState({ [prop]: event.target.value });
+        this.setState({ [prop]: event.target.value })
+        this.setState(state => ({ enableSave: true }));
     };
 
     handleClickShowPassword = () => {
-        this.setState(state => ({ readOnly: !state.readOnly }));
+        this.setState(state => ({ showPassword: !state.showPassword }));
     };
 
-    editing = () => {
-        this.setState(state => ({ enableSave: true}));
+    handleSave = () => {
+        //this.props.handleSave(this.state.formInfo);
+        alert("agregar funcionalidad de guardar datos")
     };
 
     render() {
-        //alert(this.props.area)
         const { classes } = this.props;
 
         return (
-            <Table className={classes.table}>
-                <TableBody>
-                    <TableRow>
-                        <TableCell align="left">
-                            <TextField
-                                id="name"
-                                label="Nombre"
-                                className={classes.textField}
-                                defaultValue={this.props.nombre}
-                                margin="normal"
-                                //onBlur={this.editing()}
-                            />
-                        </TableCell>
-                        <TableCell align="left">
-                            <TextField
-                                id="id"
-                                label="ID"
-                                className={classes.textField}
-                                defaultValue={this.props.id}
-                                InputProps={{
-                                    readOnly: true,
-                                }}
-                                margin="normal"
-                                //onBlur={this.editing()}
-                            />
-                        </TableCell>
-                        <td align="right">
-                            <Button variant="contained" color="primary" style={buttonStyle1}>
-                                {save()}
-                            </Button>
-                        </td>
-                    </TableRow>
-                    <TableRow>
-                        <TableCell align="left">
-                            <TextField
-                                select
-                                label="Lugar"
-                                value={this.state.lugar}
-                                onChange={this.handleChange('lugar')}
-                                InputProps={{
-                                    readOnly: this.state.readOnly,
-                                }}
-                            >
-                                {lugares.map(option => (
-                                    <MenuItem key={option.value} value={option.value}>
-                                        {option.label}
-                                    </MenuItem>
-                                ))}
-                            </TextField>
-                        </TableCell>
-                        <TableCell align="left">
-                            <TextField
-                                select
-                                label="Area"
-                                value={this.state.area}
-                                onChange={this.handleChange('area')}
-                                InputProps={{
-                                    readOnly: this.state.readOnly,
-                                }}
-                            >
-                                {areas.map(option => (
-                                    <MenuItem key={option.value} value={option.value}>
-                                        {option.label}
-                                    </MenuItem>
-                                ))}
-                            </TextField>
-                        </TableCell>
-                        <TableCell align="left" colSpan={2}>
-                            <TextField
-                                id="puesto"
-                                label="Puesto"
-                                className={classes.textField}
-                                defaultValue={this.props.puesto}
-                                InputProps={{
-                                    readOnly: this.state.readOnly,
-                                }}
-                                margin="normal"
-                                //onBlur={this.editing()}
-                            />
-                        </TableCell>
-                    </TableRow>
-                </TableBody>
-            </Table>
+            <FormControl component="fieldset" className={classes.formControl}>
+                <Table className={classes.table}>
+                    <TableBody>
+                        <TableRow>
+                            <TableCell align="left">
+                                <TextField
+                                    id="name"
+                                    label="Nombre"
+                                    className={classes.textField}
+                                    defaultValue={this.props.nombre}
+                                    margin="normal"
+                                    onChange={this.handleChange('nombre')}
+                                />
+                            </TableCell>
+                            <TableCell align="left">
+                                <TextField
+                                    id="username"
+                                    label="Username"
+                                    className={classes.textField}
+                                    defaultValue={this.props.username}
+                                    onChange={this.handleChange('username')}
+                                    InputProps={{
+                                        //readOnly: true,
+                                    }}
+                                    margin="normal"
+                                />
+                            </TableCell>
+                            <TableCell align="left">
+                                <InputLabel htmlFor="adornment-password">Password</InputLabel>
+                                <Input
+                                    id="password"
+                                    defaultValue={this.props.userpassword}
+                                    type={this.state.showPassword ? 'text' : 'password'}
+                                    value={this.state.password}
+                                    onChange={this.handleChange('password')}
+                                    endAdornment={
+                                        <InputAdornment position="end">
+                                            <IconButton
+                                                aria-label="Toggle password visibility"
+                                                onClick={this.handleClickShowPassword}
+                                            >
+                                                {this.state.showPassword ? <Visibility /> : <VisibilityOff />}
+                                            </IconButton>
+                                        </InputAdornment>
+                                    }
+                                />
+                            </TableCell>
+                            <td align="right">
+                                <Button
+                                    variant="contained"
+                                    color="primary"
+                                    disabled={!this.state.enableSave}
+                                    style={buttonStyle1}
+                                    onClick={this.handleSave}      //change
+                                >
+                                        {save()}
+                                </Button>
+                            </td>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell align="left">
+                                <TextField
+                                    select
+                                    label="Lugar"
+                                    value={this.state.lugar}
+                                    onChange={this.handleChange('lugar')}
+                                    InputProps={{
+                                        readOnly: this.state.readOnly,
+                                    }}
+                                >
+                                    {lugares.map(option => (
+                                        <MenuItem key={option.value} value={option.value}>
+                                            {option.label}
+                                        </MenuItem>
+                                    ))}
+                                </TextField>
+                            </TableCell>
+                            <TableCell align="left">
+                                <TextField
+                                    select
+                                    label="Area"
+                                    value={this.state.area}
+                                    onChange={this.handleChange('area')}
+                                    InputProps={{
+                                        readOnly: this.state.readOnly,
+                                    }}
+                                >
+                                    {areas.map(option => (
+                                        <MenuItem key={option.value} value={option.value}>
+                                            {option.label}
+                                        </MenuItem>
+                                    ))}
+                                </TextField>
+                            </TableCell>
+                            <TableCell align="left" colSpan={2}>
+                                <TextField
+                                    id="puesto"
+                                    label="Puesto"
+                                    className={classes.textField}
+                                    defaultValue={this.props.puesto}
+                                    onChange={this.handleChange('puesto')}
+                                    InputProps={{
+                                        readOnly: this.state.readOnly,
+                                    }}
+                                    margin="normal"
+                                />
+                            </TableCell>
+                        </TableRow>
+                    </TableBody>
+                </Table>
+            </FormControl>
         );
     }
 }
